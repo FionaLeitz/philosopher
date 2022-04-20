c58d709d384ddec631b6d3846972107519b54849
Fiona LEITZ <fleitz@e2r1p13.clusters.42paris.fr>
2022-04-20 13:20:56 +0200

    Makefile et main

---
+++ philo/headers/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fleitz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 10:07:50 by fleitz            #+#    #+#             */
+/*   Updated: 2022/04/20 10:07:59 by fleitz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+
+#endif
